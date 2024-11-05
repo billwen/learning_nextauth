dca73aa1663ea1d7c3a85b79a114196f95195c06
Gang Wen <73705196+gawen-ikea@users.noreply.github.com>
2024-11-05 18:44:56 +0100

    completed email verification

--- src/components/auth/email-verification-form.tsx
+++ src/components/auth/email-verification-form.tsx
@@ -3,15 +3,42 @@
 import { CardWrapper } from '@/components/auth/card-wrapper';
 import { BeatLoader } from 'react-spinners';
 import { useSearchParams } from 'next/navigation';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { verifyToken } from '@/data-service/verificaiton-token-service';
+import { FormSuccess } from '@/components/shared/form-success';
+import { FormError } from '@/components/shared/form-error';
 
 export function EmailVerificationForm() {
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [success, setSuccess] = useState<string | undefined>(undefined);
+
   const searchParams = useSearchParams();
   const token = searchParams.get('token');
 
   const onSubmit = useCallback(async () => {
-    console.log(token);
-  }, [token]);
+    // If the request has been made, then no need to make another request
+    if (error || success) {
+      // on dev environment, useEffect will be called twice, so here we need to check if the error or success is already set
+      return;
+    }
+
+    if (!token) {
+      setError('Token is required');
+      return;
+    }
+
+    verifyToken(token).then((response) => {
+      if (response.error) {
+        setError(response.error);
+      } else {
+        if (response.success) {
+          setSuccess(response.success);
+        }
+      }
+    }).catch(() => {
+      setError('Something went wrong');
+    });
+  }, [token, success, error]);
 
   useEffect(() => {
     onSubmit();
@@ -20,7 +47,11 @@ export function EmailVerificationForm() {
   return (
     <CardWrapper headerLabel="Confirmation your verification" backButtonHref="/auth/login" backButtonLabel="Back to Login">
       <div className="flex items-center justify-center w-full">
-        <BeatLoader />
+        { !error && !success && (<BeatLoader />)}
+
+        <FormSuccess message={success} />
+        {!success && (<FormError message={error} />)}
+
       </div>
     </CardWrapper>
 
