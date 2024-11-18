d153707740c962ca82492c5fbd53dfbc69782a8d
billwen <billwen@GD-SVR-MA001.guludoc.com>
2024-11-18 06:51:28 -0500

    added some files

--- docker-bootstrap-app.sh
+++ docker-bootstrap-app.sh
@@ -2,6 +2,7 @@
 # ENVIRONMENT from docker-compose.yaml doesn't get through to subprocesses
 # Need to explicit pass DATABASE_URL here, otherwise migration doesn't work
 # Run migrations
-DATABASE_URL="postgres://postgres:postgres@db:5432/appdb?sslmode=disable" npx prisma migrate deploy
+echo "Connecting to $DATABASE_URL"
+npx prisma migrate deploy
 # start app
-DATABASE_URL="postgres://postgres:postgres@db:5432/appdb?sslmode=disable" node server.js
+node server.js
